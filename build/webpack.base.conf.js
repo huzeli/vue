6a9c8a9b9b9a4ff690c933dc08b084b2904de47e
huzeli <18317131379@163.com>
2021-02-06 16:52:37 +0800

    登录页面的制作

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -67,6 +67,7 @@ module.exports = {
           name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
+
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,30 +1,98 @@
 <template>
-<div>
-    <el-form ref="form" :model="loginForm">
-        <h3>系统登录</h3>
-        <el-form-item>
-            <el-input type="text" v-model="loginForm.username" placeholder="请输入用户名"></el-input>
-        </el-form-item>
-        <el-form-item>
-        </el-form-item>
+  <div>
+    <el-form :rules="rules"
+             ref="loginForm"
+             :model="loginForm"
+             class="loginContainer">
+      <h3 class="loginTitle">系统登录</h3>
+      <el-form-item prop="username">
+        <el-input type="text"
+                  v-model="loginForm.username"
+                  placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input type="password"
+                  auto-complete="false"
+                  v-model="loginForm.password"
+                  placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item prop="code">
+        <el-input type="text"
+                  auto-complete="false"
+                  v-model="loginForm.code"
+                  placeholder="点击图片更换验证码"
+                  style="width:80%"></el-input>
+      </el-form-item>
+      <img :src="captchUrl"
+           @click="updateCapthUrl" />
+      <el-checkbox v-model="checked"
+                   class="loginRemenber">请记住我</el-checkbox>
+      <el-button type="primary"
+                 style="width:100%"
+                 @click="submitLoin">登录</el-button>
     </el-form>
-</div>
+
+  </div>
 </template>
 
 <script>
 export default {
-    name: "login",
-    data () {
-        return{
-            loginForm: {
-                username: 'admin',
-                password: '123',
-                code: ''
-            }
-        }
+  name: 'login',
+  data () {
+    return {
+      captchUrl: '/captch?time=' + new Date(),
+      loginForm: {
+        username: 'admin',
+        password: '123',
+        code: ''
+      },
+      checked: true,
+      rules: {
+        username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
+        code: [{ required: true, message: '请输入验证码', trigger: 'blur' }]
+      }
     }
+  },
+  methods: {
+    updateCapthUrl () {
+      this.captchUrl = '/captch?time=' + new Date()
+    },
+    submitLoin () {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          this.$message.error('请输入所有字段');
+          return false;
+        }
+      });
+    }
+  }
 }
 </script>
 
 <style>
+.loginContainer {
+  border-radius: 15px;
+  background-clip: padding-box;
+  margin: 180px auto;
+  width: 350px;
+  padding: 15px 35px 15px 35px;
+  background: #fff;
+  border: 1px solid #cac6c6;
+  box-shadow: 0 0 25px #cac6c6; /** 阴影 */
+}
+.loginTitle {
+  margin: 0 auto 40px auto;
+  text-align: center;
+}
+.loginRemenber {
+  text-align: left;
+  margin: 0px 0px 15px 0px;
+}
+.el-form-item__content {
+  display: flex;
+  align-items: center;
+}
 </style>
